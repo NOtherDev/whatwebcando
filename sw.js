b3498ab20e3ad0c36b01ff406de81d50b949afc4
adambar <adam.bar@brightinventions.pl>
2017-05-02 10:51:55 +0200

    NFC update (see #31)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'f532b46'; const groups = [
+const VERSION = '3325ecf'; const groups = [
   {
     "heading": "Native Behaviors",
     "features": [
@@ -233,7 +233,7 @@ this.addEventListener('install', function (event) {
       let files = [
         '/',
         '/index.html',
-        '/scripts/main.5ebb1669.js',
+        '/scripts/main.ba993360.js',
         '/scripts/vendor.f1fab690.js',
         '/styles/main.b1119fb2.css',
         '/styles/vendor.5d38139c.css',
