566e89b491c4198a96c1772981af2dad2ace533b
adambar <adam.bar@brightinventions.pl>
2017-07-02 22:59:20 +0200

    feat(ambient-light): Update availability info and API

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '64ee7ed'; const groups = [
+const VERSION = 'a9e3f01'; const groups = [
   {
     "heading": "Native Behaviors",
     "features": [
@@ -233,7 +233,7 @@ this.addEventListener('install', function (event) {
       let files = [
         '/',
         '/index.html',
-        '/scripts/main.362719c6.js',
+        '/scripts/main.119fe5a9.js',
         '/scripts/vendor.f1fab690.js',
         '/styles/main.b1119fb2.css',
         '/styles/vendor.5d38139c.css',
