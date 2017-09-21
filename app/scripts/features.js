5b7a4b8ec691517a0e5d01f0525e531bb39fe814
adambar <adam.bar@brightinventions.pl>
2017-09-21 07:57:17 +0200

    feat(network-info): navigator.connection.effectiveType

--- app/scripts/features.js
+++ app/scripts/features.js
@@ -471,11 +471,15 @@ self.addEventListener('fetch', function (event) {
     networkInfo: new Feature({
       id: 'network-type-speed',
       name: 'Network Type & Speed',
-      description: `The <b>Network Information API</b> allows web applications to read the current network type and the maximum downlink speed
-        and to subscribe for a notification when the network type has changed.`,
+      description: [`The <b>Network Information API</b> allows web applications to read the current network type and the maximum downlink speed that are assumed
+        based on the underlying connection technology used by the client. It also allows to subscribe for a notification when the network type has changed.`,
+        `The newest addition (mid-2017), implemented by Chrome on Android as of September 2017, also provides an effective network type information that in turn
+        is calculated using the actual network performance metrics collected by the browser, allowing the web applications to adjust to the quality of the connection.`],
       api: `<dl>
         <dt><code>navigator.connection.type</code></dt>
-        <dd>Returns the type of the current connection, i.e. <code>cellular</code>, <code>wifi</code>, <code>none</code> etc.</dd>
+        <dd>Returns the theoretical type of the current connection, based on the underlying connection technology, i.e. <code>cellular</code>, <code>wifi</code>, <code>none</code> etc.</dd>
+        <dt><code>navigator.connection.effectiveType</code></dt>
+        <dd>Returns the information about the quality of the current connection based on recently observed performance metrics, regardless of the underlying connection technology, i.e. <code>slow-2g</code>, <code>2g</code>, <code>3g</code>, <code>4g</code>.</dd>
         <dt><code>navigator.connection.downlinkMax</code></dt>
         <dd>Returns the theoretical maxinum downlink speed, in Mbps, for the underlying technology of the current connection.</dd>
         <dt><code>navigator.connection.addEventListener('change', listener)</code></dt>
@@ -485,7 +489,7 @@ self.addEventListener('fetch', function (event) {
       caniuse: 'netinfo',
       demoPen: 'LpWPvv',
       links: [
-        {url: 'https://w3c.github.io/netinfo/', title: 'Specification Draft'},
+        {url: 'https://wicg.github.io/netinfo/', title: 'Specification Draft'},
         {url: 'https://developer.mozilla.org/en-US/docs/Web/API/Network_Information_API', title: 'MDN: Network Information API'}
       ]
     }),
