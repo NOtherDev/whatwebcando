c2049dc1a011198b8ce66a540345729cd86dce6d
adambar <adam.bar@brightinventions.pl>
2017-11-16 22:08:18 +0100

    Credential Management API added

--- app/scripts/features.js
+++ app/scripts/features.js
@@ -3046,8 +3046,102 @@ function donate() {
       },
       links: [
         {url: 'https://www.w3.org/TR/payment-request/', title: 'Specification'},
-        {url: 'https://developers.google.com/web/fundamentals/payments/', title: 'Google Develpers: Introducing the Payment Request API'},
+        {url: 'https://developers.google.com/web/fundamentals/payments/', title: 'Google Developers: Introducing the Payment Request API'},
         {url: 'https://developers.google.com/web/fundamentals/payments/deep-dive-into-payment-request', title: 'Google Developers: Deep Dive into the Payment Request API'}
+      ]
+    }),
+
+    credentials: new Feature({
+      id: 'credentials',
+      name: 'Credentials',
+      description: [`The <b>Credential Management API</b> allows authorized Web applications to store and request user credentials (like login and password
+        or federated login data) programmatically on behalf of the user. The API offers a replacement for browser built-in or 3rd-party password stores
+        which allows the Web application to instrument when and how the credentials are stored and read, for example to offer automatic sign-in capability.`,
+      `When retrieving the previously stored credential, the Web application may decide if the user mediation is expected. When <code>mediation</code>
+       flag is set to <code>silent</code>, the credential will only be resolved if the user had previously stored the single credential for this application.
+       With <code>optional</code> option set, the UI to choose the credential will appear unless there's a single credential stored. Finally, with
+       <code>required</code> option, the UI will always be shown, regardless of the previously stored credentials existence.`],
+      api: `<dl>
+        <dt><code>credential = new PasswordCredential(form)</code></dt>
+        <dd>Creates the credential object based on the username and password data detected in a HTML <code>&lt;form&gt;</code> element.</dd>
+        <dt><code>credential = new PasswordCredential({id, password, name, iconURL})</code></dt>
+        <dd>Creates the credential object manually based on the data provided.</dd>
+        <dt><code>credential = new FederatedCredential({id, name, iconURL, provider, protocol})</code></dt>
+        <dd>Creates the credential object based on the federated login service specified with <code>provider</code> and <code>protocol</code> options.</dd>
+        <dt><code>navigator.credentials.store(credential)</code></dt>
+        <dd>Stores the credential provided for future access. Returns a <code>Promise</code> resolved when the credential is successfully persisted by the browser.</dd>
+        <dt><code>navigator.credentials.get({mediation})</code></dt>
+        <dd>Retrieves the previously stored credential from the browser, optionally with user mediation (UI). Returns a <code>Promise</code> resolved with fetched credential or <code>null</code>.</dd>
+        <dt><code>navigator.credentials.preventSilentAccess()</code></dt>
+        <dd>Ensures the user mediation is required on the next credential access request, effectively "logging out" the user.</dd>
+      </dl>`,
+      tests: [Feature.navigatorContains('credentials')],
+      caniuse: 'credential-management',
+      demo: {
+        html: `<form id="credential-form">
+  <p><b>Store your credentials:</b></p>
+  <p>
+    <label>login: <input type="text" name="username" class="form-control" required autocomplete="username"></label>
+    <label>password: <input type="password" name="password" class="form-control" required autocomplete="current-password"></label>
+  </p>
+  <p><button type="button" onclick="storeCredential()" class="btn btn-default">Store credential</button></p>
+
+  <p class="user-mediation">
+    <b>User mediation:</b>
+    <label><input type="radio" name="mediation" value="silent"> silent</label>
+    <label><input type="radio" name="mediation" value="optional" checked> optional</label>
+    <label><input type="radio" name="mediation" value="required"> required</label>
+  </p>
+  <p><button type="button" onclick="requestCredential()" class="btn btn-default">Request credential</button></p>
+  <p>Credential read: <b id="readResult">none</b>.</p>
+  <p><button type="button" onclick="preventSilentAccess()" class="btn btn-default">Prevent silent access (logout)</button></p>
+</form>`,
+        js: `function storeCredential() {
+  if (!navigator.credentials) {
+    alert('Credential Management API not supported');
+    return;
+  }
+  
+  let credentialForm = document.getElementById('credential-form');
+  let credential = new PasswordCredential(credentialForm);
+  navigator.credentials.store(credential);
+}
+
+function requestCredential() {
+  if (!navigator.credentials) {
+    alert('Credential Management API not supported');
+    return;
+  }
+  
+  let mediationValue = document.getElementById('credential-form').mediation.value;
+  navigator.credentials.get({password: true, mediation: mediationValue})
+    .then(credential => {
+      let result = 'none';
+      if (credential) {
+        result = credential.id + ', ' + credential.password.replace(/./g, '*');
+      }
+      document.getElementById('readResult').innerText = result;
+    })
+    .catch(err => alert('Error reading credentials: ' + err));
+}
+
+function preventSilentAccess() {
+  if (!navigator.credentials) {
+    alert('Credential Management API not supported');
+    return;
+  }
+  
+  navigator.credentials.preventSilentAccess();
+}`,
+        cssHidden: `.user-mediation label {
+  margin-left: 1em;
+}`
+      },
+      links: [
+        {url: 'https://w3c.github.io/webappsec-credential-management/', title: 'Specification Draft'},
+        {url: 'https://developers.google.com/web/updates/2016/04/credential-management-api', title: 'Google Developers: Streamlining the Sign-in Flow Using Credential Management API'},
+        {url: 'https://medium.com/dev-channel/sign-in-on-the-web-credential-management-api-and-best-practices-d21aed14b6fe', title: 'Sign-in on the Web — Credential Management API and Best Practices'},
+        {url: 'https://polykart-credential-payment.appspot.com/', title: 'Auto sign-in demo'}
       ]
     })
   };
