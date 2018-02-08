bf1d85da9d09e03ba469338f30994e41dc7d9e75
adambar <adam.bar@brightinventions.pl>
2018-02-08 13:14:47 +0100

    `supportedMethods` is not an array anymore (#44)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '6641512'; const groups = [
+const VERSION = 'ba5fa22'; const groups = [
   {
     "heading": "Native Behaviors",
     "features": [
@@ -272,7 +272,7 @@ self.addEventListener('install', function (event) {
       let files = [
         '/',
         '/index.html',
-        '/scripts/main.2c76b20a.js',
+        '/scripts/main.181f72e6.js',
         '/scripts/vendor.65ca8f8f.js',
         '/styles/main.9afc3c79.css',
         '/styles/layout.60a43676.css',
