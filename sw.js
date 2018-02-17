e0a11cbdc815e97379070a83af8849bc03f03f99
adambar <adam.bar@brightinventions.pl>
2018-02-17 22:29:24 +0100

    More comprehensive demo for Permissions API

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '15cf13f'; const groups = [
+const VERSION = '3673218'; const groups = [
   {
     "heading": "Native Behaviors",
     "features": [
@@ -272,7 +272,7 @@ self.addEventListener('install', function (event) {
       let files = [
         '/',
         '/index.html',
-        '/scripts/main.b394e265.js',
+        '/scripts/main.ff64b781.js',
         '/scripts/vendor.65ca8f8f.js',
         '/styles/main.9afc3c79.css',
         '/styles/layout.60a43676.css',
