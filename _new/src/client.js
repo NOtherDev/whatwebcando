774a69fab0b691d5e3263096f522d9e07a991a46
adambar <hi@adambar.pl>
2020-01-03 21:03:15 +0100

    Include Google Analytics with sending pageviews

--- _new/src/client.js
+++ _new/src/client.js
@@ -1,5 +1,28 @@
 import * as sapper from '@sapper/app';
 
 sapper.start({
-	target: document.querySelector('#sapper')
+  target: document.querySelector('#sapper')
 });
+
+const googleAnalytics = (gaID) => {
+  window.dataLayer = window.dataLayer || []
+
+  function gtag() {
+    dataLayer.push(arguments)
+  }
+
+  gtag('js', new Date())
+
+  window.gaPageView = function (path) {
+    gtag('config', gaID, {
+      'page_path': path,
+      'anonymize_ip': true,
+    })
+  }
+
+  const script = document.createElement('script')
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaID}`
+  document.body.appendChild(script)
+}
+
+googleAnalytics('UA-69397212-1')
--- _new/src/routes/_layout.svelte
+++ _new/src/routes/_layout.svelte
@@ -1,8 +1,17 @@
 <script>
-	import Header from '../components/Header.svelte';
-	import Footer from '../components/Footer.svelte';
+  import { stores } from '@sapper/app';
+  import Header from '../components/Header.svelte';
+  import Footer from '../components/Footer.svelte';
 
-	export let segment;
+  export let segment;
+
+  const { page } = stores();
+
+  page.subscribe(({ path }) => {
+    if (process.browser && window.gaPageView) {
+      window.gaPageView(path)
+    }
+  })
 </script>
 
 <style type="text/scss" global>
