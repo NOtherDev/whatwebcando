dfb24e9b1738fe0df518f9dc068cb685b05f679a
adambar <hi@adambar.pl>
2020-01-13 14:04:44 +0100

    Update Web NFC feature (by @beaufortfrancois in #58)

--- _new/src/data/features/nfc.js
+++ _new/src/data/features/nfc.js
@@ -4,86 +4,77 @@ export default new Feature({
   id: 'nfc',
   name: 'NFC',
   description: [
-    `The <b>Web NFC API</b> is a low-level API allowing Web applications to access and exchange the data with the Near-Field Communication devices, such as NFC tags.`,
-    `It allows setting up a watch that informs when the NFC tag matching the <code>options</code> has appeared in the device sensor proximity. It also provides a method to push a message via NFC interface.`,
-    `Current support is limited to an experimental implementation in Chrome, available behind an "Experimental Web Platform Features" on Android. There was also Firefox OS experimental implementation that is <code>moz</code>-prefixed and doesn't follow the current state of the specification draft.`
+    `The <b>Web NFC API</b> is a low-level API that provides sites the ability to read and write to nearby NFC (Near-Field Communication) devices.`,
+    `It allows starting up a scan that informs when an NFC tag matching some <code>options</code> has been tapped. It also provides a method to push a message via NFC.`,
+    `Current support is limited to an experimental implementation in Chrome, available behind the "enable-webnfc" flag on Android. There was also Firefox OS experimental implementation that is <code>moz</code>-prefixed and doesn't follow the current state of the specification draft.`
   ],
   api: `<dl>
-        <dt><code>navigator.nfc.watch(callback, options)</code></dt>
-        <dd>Registers for a notification about the data read from the NFC adapter specified by <code>options</code>.</dd>
-        <dt><code>navigator.nfc.push(message, options)</code></dt>
-        <dd>Triggers sending the <code>message</code> (string, <code>ArrayBuffer</code> or NDEF record structure) to the NFC adapter specified by <code>options</code>.</dd>
+        <dt><code>const reader = new NDEFReader()</code></dt>
+        <dd>Creates an object used for NDEF readings.</dd>
+        <dt><code>reader.start(options)</code></dt>
+        <dd>Returns a <code>Promise</code> resolved if starting NFC scan was successful.</dd>
+        <dt><code>reader.addEventListener('reading', listener)</code></dt>
+        <dd>An event fired when a new reading is available.</dd>
+        <dt><code>reader.addEventListener('error', listener)</code></dt>
+        <dd>An event fired when an error happened during reading.</dd>
+        <dt><code>const writer = new NDEFWriter()</code></dt>
+        <dd>Creates an object used for NDEF writings.</dd>
+        <dt><code>writer.push(message, options)</code></dt>
+        <dd>Returns a <code>Promise</code> resolved if pushing the <code>message</code> (String, ArrayBuffer or NDEF record) with <code>options</code> was successful.</dd>
       </dl>`,
-  tests: [Feature.navigatorContains('nfc')],
+  tests: [
+    Feature.windowContains('NDEFReader'),
+    Feature.windowContains('NDEFWriter')
+  ],
   demo: {
     html: `<p>
-  <button onclick="readWriteNfc()">Test NFC Read/Write</button>
+  <button onclick="readTag()">Test NFC Read</button>
+  <button onclick="writeTag()">Test NFC Write</button>
 </p>
-
 <pre id="log"></pre>
-
 <p><small>Based on the code snippets from <a href="https://w3c.github.io/web-nfc/#examples">specification draft</a>.</small></p>`,
-    js: `function readWriteNfc() {
-  if ('nfc' in navigator) {
-    navigator.nfc.watch(function (message) {
-        consoleLog("NFC message received from URL " + message.url);
-        if (message.data[0].recordType === 'empty') {
-          navigator.nfc.push([{
-            url: message.url,
-            data: [{
-              recordType: "text",
-              data: 'Hello World'
-            }]
-          }]);
+    js: `async function readTag() {
+  if ("NDEFReader" in window) {
+    const reader = new NDEFReader();
+    try {
+      await reader.scan();
+      reader.onreading = event => {
+        const decoder = new TextDecoder();
+        for (const record of event.message.records) {
+          consoleLog("Record type:  " + record.recordType);
+          consoleLog("MIME type:    " + record.mediaType);
+          consoleLog("=== data ===\\n" + decoder.decode(record.data));
         }
-        processMessage(message);
-      }, {mode: 'any'})
-      .then(() => consoleLog("Added a watch."))
-      .catch(err => consoleLog("Adding watch failed: " + err.name));
+      }
+    } catch(error) {
+      consoleLog(error);
+    }
   } else {
-    consoleLog('NFC API not supported.');
+    consoleLog("Web NFC is not supported.");
+  }
+}
+
+async function writeTag() {
+  if ("NDEFWriter" in window) {
+    const writer = new NDEFWriter();
+    try {
+      await writer.push("What Web Can Do Today");
+      consoleLog("NDEF message pushed!");
+    } catch(error) {
+      consoleLog(error);
+    }
+  } else {
+    consoleLog("Web NFC is not supported.");
   }
 }
 
 function consoleLog(data) {
   var logElement = document.getElementById('log');
-  logElement.innerHTML += '\\n' + data;
-}
-
-function processMessage(message) {
-  message.data.forEach(function (record) {
-    if (record.recordType == "string") {
-      consoleLog('Data is string: ' + record.data);
-    } else if (record.recordType == "json") {
-      processJSON(record.data);
-    } else if (record.recordType == "url") {
-      consoleLog("Data is URL: " + record.data);
-    } else if (record.recordType == "opaque" && record.mediaType == 'image/png') {
-      processPng(record.data);
-    };
-  });
-}
-
-function processPng(data) {
-  consoleLog("Known image/png data");
-
-  var img = document.createElement("img");
-  img.src = URL.createObjectURL(new Blob(data, 'image/png'));
-  img.onload = function () {
-    window.URL.revokeObjectURL(this.src);
-  };
-};
-
-function processJSON(data) {
-  var obj = JSON.parse(data);
-  consoleLog("JSON data: " + obj.myProperty.toString());
+  logElement.innerHTML += data + '\\n';
 };`
   },
   links: [
     {url: 'https://w3c.github.io/web-nfc/', title: 'Specification Draft'},
-    {
-      url: 'https://developer.mozilla.org/en-US/docs/Web/API/NFC_API/Using_the_NFC_API',
-      title: 'MDN: Using the NFC API (covers outdated spec revision)'
-    }
+    {url: 'https://developer.mozilla.org/en-US/docs/Web/API/NFC_API/Using_the_NFC_API', title: 'MDN: Using the NFC API (covers outdated spec revision)'}
   ]
 })
