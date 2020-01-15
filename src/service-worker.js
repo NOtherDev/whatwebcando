3c47ba35f286bf0d64d6f0cebaadcfbd9ecbfe98
adambar <hi@adambar.pl>
2020-01-15 14:33:56 +0100

    Get rid of CNAME from Service Worker cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,6 +12,7 @@ const toCache = shell.concat(files)
   .filter((f) => !f.endsWith('.DS_Store'))
   .filter((f) => !f.startsWith('articleimgs/'))
   .filter((f) => f !== 'images/share-image.png')
+  .filter((f) => f !== 'CNAME')
   .concat([
     'https://fonts.gstatic.com/s/sourcesanspro/v13/6xK3dSBYKcSV-LCoeQqfX1RYOo3qOK7lujVj9w.woff2',
     'https://fonts.gstatic.com/s/sourcesanspro/v13/6xKydSBYKcSV-LCoeQqfX1RYOo3ig4vwlxdu3cOWxw.woff2',
