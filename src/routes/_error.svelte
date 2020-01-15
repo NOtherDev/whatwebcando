11f491dd027f492710cb664f2ae5451633f276b2
adambar <hi@adambar.pl>
2020-01-15 14:00:39 +0100

    Recover from dynamic loading errors after deployment

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,10 +1,30 @@
 <script>
+  import {afterUpdate} from 'svelte';
   import Meta from '../components/Meta.svelte';
 
 	export let status;
 	export let error;
 
 	const dev = process.env.NODE_ENV === 'development';
+
+	if (process.browser) {
+    afterUpdate(async () => {
+      // dynamic module import might fail with undefined or "failed to fetch" error when no valid SW -
+      // this means that probably the newer version was deployed in the meantime and there's nowhere to fetch the code from
+      // all we can hope is that refresh fixes it
+      if (error && status === 500 && (!error.message || error.message.startsWith("Failed to fetch"))) {
+        // one unlikely but possible cause is that SW is broken for some reason - let's get rid of it
+        if ('serviceWorker' in navigator) {
+          const registration = await navigator.serviceWorker.getRegistration()
+          if (registration) {
+            await registration.unregister()
+          }
+        }
+
+        window.location.reload(true)
+      }
+    })
+  }
 </script>
 
 <svelte:head>
