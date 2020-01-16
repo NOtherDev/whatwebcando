7e98912c9c706ed6cdc8afe4e046be5cdd32a04d
adambar <hi@adambar.pl>
2020-01-16 07:59:37 +0100

    Add rel="noopener" to external links - fix typo

--- src/data/features/memory.js
+++ src/data/features/memory.js
@@ -7,7 +7,7 @@ export default new Feature({
         It might be used to identify the lower-end devices to provide the reduced, lightweight experience of the website for performance reasons.
         The value provided by the API does not hint how much of the memory is actually available for the application to use - its purpose is only
         to serve as a device class indication.`,
-    `The API consists of two parts. The first is the memory size exposed via the JavaScript property. The second is the <a href="http://httpwg.org/http-extensions/client-hints.html" target="_blank rel="noopener">Client Hint</a> mechanism of the browser
+    `The API consists of two parts. The first is the memory size exposed via the JavaScript property. The second is the <a href="http://httpwg.org/http-extensions/client-hints.html" target="_blank" rel="noopener">Client Hint</a> mechanism of the browser
         that sends the total device memory information as <code>Device-Memory</code> HTTP request header when the server previously opted-in
         to receiving this information with <code>Accept-CH: Device Memory</code> HTTP response header, so that the server side might decide to serve 
         the content optimized for the particular class of device.`],
