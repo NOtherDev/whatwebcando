aa35786ad78ab610f6444d00749449d4c929f10d
adambar <hi@adambar.pl>
2020-03-06 11:51:56 +0100

    Remove training advertisement

--- src/components/PromoBox.svelte
+++ src/components/PromoBox.svelte
@@ -1,34 +1,3 @@
-<script>
-  import {onMount} from 'svelte';
-
-  if (process.browser) {
-    onMount(() => {
-      const $ad = document.querySelector('.ad')
-
-      if ($ad && (navigator.language || '').toLowerCase().includes('pl')) {
-        $ad.innerHTML = `<div id="carbonads" style="background: var(--primary-background); color: black;">
-          <span class="carbon-wrap">
-            <span>
-            <strong>Chcesz zostać ekspertem Mobile Weba?</strong><br/>
-            🔥 Szkolenie: Progresywne Aplikacje Webowe<br/>
-            🌍 Warszawa<br/>
-            📅 25-27 marca 2020<br/>
-            👨‍🏫 <a href="https://adambar.pl/pl.html" target="_blank" rel="noopener">Adam Bar</a>, autor What Web Can Do Today
-            </span>
-          </span>
-          <span class="carbon-wrap" style="justify-content: center">
-            <a href="https://bottega.com.pl/nabor-otwarty-progresywne-aplikacje-web" target="_blank" rel="noopener"
-              class="button"
-              style="margin-top: .5em; padding: .5em 1.5em">
-              Zapisz się na szkolenie
-            </a>
-          </span>
-        </div>`
-      }
-    })
-  }
-</script>
-
 <style>
   h2 {
     font-size: 1.125em;
