ee99c1db6dadb774f4a9b4dedf04c34c2aa5fd19
adambar <hi@adambar.pl>
2022-03-02 22:24:57 +0100

    Use commit hash in message, remove old methods

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,22 @@
 const ghpages = require('gh-pages');
+const { exec } = require("child_process");
 
-ghpages.publish(
+exec("git rev-parse HEAD", (error, revHash) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  ghpages.publish(
     '__sapper__/export',
     {
-        branch: 'gh-pages',
-        remote: 'origin',
-        message: 'Deploy on GitHub Pages via gh-pages script',
+      branch: 'gh-pages',
+      remote: 'origin',
+      message: `Update to ${revHash.trim()}`,
+      add: true,
     },
     () => {
-        console.log('Deploy on GitHub Pages Complete!')
+      console.log('Deploy on GitHub Pages Complete!')
     }
-)
+  )
+})
